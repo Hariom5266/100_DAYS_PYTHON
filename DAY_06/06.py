5b8394b5c3e9b839a6d6737e01c07c2ed03bc7c5
hariom joshi <159013690+Hariom5266@users.noreply.github.com>
2024-07-26 01:26:24 +0800

    Add files via upload
    🍵, Hanji Kaise ho aap sabhi ,This is 6th day of Pyhton Challenge.I’m back and ready to code,Let's roll the code!

---
+++ DAY_06/06.py
@@ -0,0 +1,11 @@
+# 🍵 , Hanji Kaise ho aap sabhi this is 6th day of #100Days_Of_Python I'm back and ready to code.let's start to do code.
+
+# ======================== fucntions ======================== #
+def my_function():
+    print("Hanji, I am user Define fucntion.")
+my_function()
+
+# =================== it is hurdle code =================== #
+# rebord's world
+
+
