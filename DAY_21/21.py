593f6b1a36a342a35c3701c89f3ab7b43d4f4b2a
hariom joshi <159013690+Hariom5266@users.noreply.github.com>
2024-08-07 21:03:51 +0530

    Add files via upload
    # 🍵 , Hanji Kaise ho aap sabhi this is 21th day of #100Days_Of_Python I'm back and ready to code.let's start to do code.

--- DAY_21/21.py
+++ DAY_21/21.py
@@ -1,7 +1,40 @@
 # 🍵 , Hanji Kaise ho aap sabhi this is 21th day of #100Days_Of_Python I'm back and ready to code.let's start to do code.
 
+# ==================== Inheritance ==================== #
 
+# we program chef robot it has bake(),stir(), and measure() like fnc
+# then we won't to program pasty chef it has much fnc of chef but also have another fnc like bake(), stir(), measure(), knead(), whisk()
 
+# class fish(animal): => here fish inhertiede from animal
+#     def __init__(self):
+#         super().__init__()
+
+class Animal:
+    def __init__(self):
+        self.num_eyes=2
+    
+    def breathe(self):
+        print("Inhale,Exhale.")
+        
+class Fish(Animal):
+    def __init__(self):
+        super().__init__()
+    
+    def breathe(self):
+         super().breathe()
+         print("Doing this in underwater")
+    
+    def swim(self):        
+        print("Moving in water.")
+        
+nemo = Fish()
+nemo.swim()
+nemo.breathe()
+print(nemo.num_eyes)
+
+# ==================== Slicing ==================== #
+piano_keys=['a','b','c','d','e','f','g']
+print(piano_keys[2:5])
 
 
 
--- DAY_21/project_21.py
+++ DAY_21/project_21.py
@@ -1,10 +1,59 @@
 # 🍵 , Hanji Kaise ho aap sabhi this is 21th day of #100Days_Of_Python I'm back and ready to code.let's start to do code.
 
 
+from turtle import Screen
+from snake import Snake
+from food import Food
+from score import Score
+import time
 
+screen=Screen()
+screen.setup(width=600,height=600)
+screen.bgcolor("black")
+screen.title("My Snack Game")
+screen.tracer(0)
 
+snake=Snake()
+food=Food()
+scoreboard=Score()
 
+screen.listen()
+screen.onkey(snake.up,"Up")
+screen.onkey(snake.down,"Down")
+screen.onkey(snake.left,"Left")
+screen.onkey(snake.right,"Right")
 
+game_is_on=True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    
+    snake.move()
+    
+    # Detect collision with food.
+    
+    if snake.head.distance(food)<20:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
 
+    # Detect collison with wall.
+    if snake.head.xcor()>280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on=False
+        scoreboard.game_over()
+        
+    # Detect collison with tail.
+    # If head collison with any segment in the tail:
+    # trigger game_over
+    
+    for segment in snake.segments[1:]:
+        # if segment==snake.head:
+        #     pass
+        if snake.head.distance(segment)<10:
+            game_is_on=False
+            scoreboard.game_over()
+            
+
+screen.exitonclick()
 
 
