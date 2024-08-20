df5f7434754a5bdcb83766c7dff37485ab3456fa
hariom joshi <159013690+Hariom5266@users.noreply.github.com>
2024-08-20 22:03:00 +0530

    Add files via upload
    # 🍵 , Hanji Kaise ho aap sabhi this is 34th day of #100Days_Of_Python I'm back and ready to code.let's start to do code.

---
+++ DAY_34/34.py
@@ -0,0 +1,19 @@
+# 🍵 , Hanji Kaise ho aap sabhi this is 34th day of #100Days_Of_Python I'm back and ready to code.let's start to do code.
+
+# -------------------- MAKE A QUIZ APP -------------------- #
+
+height : float
+quiz:bool
+weight : int
+hi:str
+
+# Type Hints
+
+def polic_check(age:int)->bool: # it is return type
+    if age>18:
+        can_drive = True
+    else:
+        can_drive = False
+    return can_drive
+
+print(polic_check(12))
