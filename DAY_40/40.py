b7aa353d684a812aa162f9d30d7ff4ac1b53e484
hariom joshi <159013690+Hariom5266@users.noreply.github.com>
2024-08-28 11:36:43 +0530

    Add files via upload
    # 🍵 , Hanji Kaise ho aap sabhi this is 40th day of #100Days_Of_Python I'm back and ready to code.let's start to do code.

---
+++ DAY_40/40.py
@@ -0,0 +1,25 @@
+# 🍵 , Hanji Kaise ho aap sabhi this is 40th day of #100Days_Of_Python I'm back and ready to code.let's start to do code.
+
+# -------------------- CAPSTONE PROJECT -- PART 2  -------------------- #
+
+#STEPS : 
+# jackes flight club --- make like that website
+
+# Text Based App:
+
+# Welcome to Hariom's Flight Club
+# We find the best flight deals and email you.
+# What is your first name?
+# What is your last name?
+# What is your email?
+# Type your email again.
+# You're in the clube !
+# Connection closed abruptly
+
+
+# In Flight Price Spreadsheet has another sheet Users
+# FirstName                   LastName                Email
+# Anglena                     Yu                      anglena@gmail.com
+
+
+
