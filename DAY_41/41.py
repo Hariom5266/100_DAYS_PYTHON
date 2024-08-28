05d10d85511fcabdb1969a2a90abecdf07802b70
hariom joshi <159013690+Hariom5266@users.noreply.github.com>
2024-08-28 11:37:14 +0530

    Add files via upload
    # 🍵 , Hanji Kaise ho aap sabhi this is 41th day of #100Days_Of_Python I'm back and ready to code.let's start to do code.

---
+++ DAY_41/41.py
@@ -0,0 +1,23 @@
+# 🍵 , Hanji Kaise ho aap sabhi this is 41th day of #100Days_Of_Python I'm back and ready to code.let's start to do code.
+
+# -------------------- WEB FOUNDATION  -------------------- #
+
+# Bowser(Client) -- ISP(Internet Services Provider) -- DNS(Domain Name System)
+
+# DNS -- like Contactbook it search ip address of that request and it database Find the ip address and the give datafile
+# Nslookup.io -- give excat ip address of any server and website
+
+# How Website work
+# Code Files
+# -- HTML -- Structure of web
+# -- CSS -- Design of Web
+# -- JS -- Funcnalities of web
+
+# Techlaunch.com -- for tech new news
+# Understand chrome devloper tool
+
+# What is HTML -- SEE index.html
+
+
+
+
